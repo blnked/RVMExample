c69a5b7a8a3fd88f5773428211ec51f3c05e6fe2
Eduard Beleninik <eduard.beleninik@me.com>
2014-05-07 18:10:15 +0300

    Initial Commit

---
+++ RVMExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RVMExample
+//
+//  Created by Eduard Beleninik on 5/7/14.
+//  Copyright (c) 2014 Eduard Beleninik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
